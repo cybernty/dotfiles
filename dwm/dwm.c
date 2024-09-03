1850d10099a6501c7d58323e50f362e956482cc7
cybernty <cybernty@gmail.com>
2024-09-03 23:39:59 +0800

    Change master window to the right of the screen

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1701,12 +1701,12 @@ tile(Monitor *m)
 	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
 			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
-			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+			resize(c, m->wx + (m->ww - mw), m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0); // Position master clients on the righ
 			if (my + HEIGHT(c) < m->wh)
 				my += HEIGHT(c);
 		} else {
 			h = (m->wh - ty) / (n - i);
-			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
+			resize(c, m->wx, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0); // Position stack clients on the left
 			if (ty + HEIGHT(c) < m->wh)
 				ty += HEIGHT(c);
 		}
