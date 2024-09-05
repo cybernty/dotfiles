44ffaae8f16b1dbdff4a5dbd855d9a5fbfc5e386
cybernty <cybernty@gmail.com>
2024-09-05 23:44:59 +0800

    Add configure file

---
+++ auto-configure.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+
+help() {
+    cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [options]
+
+Auto configure Arch Linux
+
+Available options:
+  -h, --help      Print this help and exit
+EOF
+}
+
+
+parse_args() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -h|--help)
+                help
+                exit 0
+                ;;
+            *)
+                echo "Unknown option: $1"
+                help
+                exit 1
+                ;;
+        esac
+    done
+}
+
+
+base() {
+    systemctl enable dhcpcd.service
+    systemctl start dhcpcd.service
+    systemctl status dhcpcd.service
+    ping -c3 www.google.com
+
+    sed -i 's/#Color/Color/' /etc/pacman.conf
+    pacman -Syu
+    pacman -S base-devel \
+                fastfetch \
+                git \
+                htop \
+                man \
+                neovim \
+                openssh \
+                zsh
+    ln -s /usr/bin/nvim /usr/bin/vi
+
+    useradd -m -G wheel -s /bin/zsh develop
+    passwd develop
+    visudo
+    reboot
+}
+
+
+main() {
+    parse_args "$@"
+    base
+}
+
+
+main "$@"
---
+++ install.sh
@@ -0,0 +1,31 @@
+ping -c3 archlinux.org
+
+timedatectl
+
+fdisk -l
+cfdisk
+
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
+
+pacstrap -K /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+passwd
+
+pacman -S grub amd-ucode os-prober dhcpcd
+echo $?
+
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+umount -R /mnt
+reboot
+
