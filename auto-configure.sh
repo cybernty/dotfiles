2ae7ece5317cf7d19d4ef90e1a15d5de5240a884
cybernty <cybernty@gmail.com>
2024-09-10 21:09:36 +0800

    update: auto-configure.sh

--- auto-configure.sh
+++ auto-configure.sh
@@ -80,7 +80,7 @@ window_manager() {
 
     make
     cp .xinitrc ~/.xinitrc
-    startx
+    # startx
 }
 
 
@@ -98,8 +98,91 @@ Icon=dwm
 Type=XSession
 EOF
 
-    lightdm --test-mode --debug
+    # lightdm --test-mode --debug
     sudo systemctl enable lightdm.service
+}
+
+
+aur() {
+    git clone https://aur.archlinux.org/yay.git --depth 1 && cd yay
+    makepkg -si
+    yay --version
+    pacman -Qs yay
+}
+
+
+vscode() {
+    yay -S visual-studio-code-bin
+
+    code --install-extension ms-azuretools.vscode-docker
+}
+
+
+_git() {
+    git config --global user.name local
+    git config --global user.email local
+    git config --global init.defaultBranch main
+
+    ssh-keygen -f ~/.ssh/github
+cat <<EOF > ~/.ssh/config
+Host github.com
+    HostName github.com
+    IdentityFile ~/.ssh/github
+EOF
+
+    yay lazygit
+}
+
+
+browser() {
+    sudo pacman -S chromium
+}
+
+
+input_method() {
+    sudo pacman -S fcitx5-im
+    sudo pacman -S fcitx5-chinese-addons
+    sudo nvim /etc/environment
+    # GTK_IM_MODULE=fcitx
+    # QT_IM_MODULE=fcitx
+    # XMODIFIERS=@im=fcitx
+    reboot
+    fcitx5-configtool
+    echo 'fcitx5 -d' >> ~/.zshrc
+}
+
+
+dev_env() {
+    sudo pacman -S jdk21-openjdk maven intellij-idea-community-edition
+
+    # - [Java - ArchWiki](https://wiki.archlinux.org/title/Java#Impersonate_another_window_manager)
+    #     - [Java - Arch Linux 中文维基](https://wiki.archlinuxcn.org/wiki/Java)
+    # - [Arch Linux - Package Search](https://archlinux.org/packages/?name=wmname)
+    # - [tools | suckless.org software that sucks less](https://tools.suckless.org/x/wmname/)
+    sudo pacman -S wmname
+    wmname LG3D
+    idea
+}
+
+
+clis() {
+    sudo pacman -S tree netcat dua-cli
+    yay unzip
+    yay zellij
+    yay fd
+
+    yay yazi
+    yay fzf
+    yay alacritty
+    yay tmux
+
+    mv ~/.config/nvim{,.bak}
+    git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+
+    yay bat
+    yay conky
+    yay polybar
+    yay pandoc
 }
 
 
@@ -113,8 +196,14 @@ main() {
         shell
         window_manager
         display_manager
+        aur
+        vscode
+        _git
+        browser
+        input_method
+        dev_env
+        clis
     fi
-
 }
 
 
--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# - [Java - ArchWiki](https://wiki.archlinux.org/title/Java#Impersonate_another_window_manager)
-#     - [Java - Arch Linux 中文维基](https://wiki.archlinuxcn.org/wiki/Java)
-# - [Arch Linux - Package Search](https://archlinux.org/packages/?name=wmname)
-# - [tools | suckless.org software that sucks less](https://tools.suckless.org/x/wmname/)
-sudo pacman -S wmname
-wmname LG3D
-idea
